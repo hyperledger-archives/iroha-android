33f78a0dd0455f6aa68e9140fd89ad29d32cf1c1
kobayashi_kento <kobaken0029@gmail.com>
2016-09-25 03:30:40 +0900

    Added network mock util

---
+++ app/src/test/java/io/soramitsu/iroha/utils/NetworkMockUtil.java
@@ -0,0 +1,29 @@
+package io.soramitsu.iroha.utils;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockWebServer;
+
+
+public class NetworkMockUtil {
+    private static MockWebServer mockWebServer;
+
+    private NetworkMockUtil() {
+    }
+
+    public static void createMockWebServer(Dispatcher dispatcher) throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.setDispatcher(dispatcher);
+        mockWebServer.start();
+    }
+
+    public static void shutdownMockWebServer() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    public static HttpUrl call(String url) {
+        return mockWebServer.url(url);
+    }
+}
