1e1d5301b94f44c8f9d585bef4a5c3c1927ed3fa
kobayashi_kento <kobaken0029@gmail.com>
2016-09-24 09:54:05 +0900

    Implemented base client

--- app/src/main/java/io/soramitsu/iroha/models/apis/BaseClient.java
+++ app/src/main/java/io/soramitsu/iroha/models/apis/BaseClient.java
@@ -1,13 +1,71 @@
 package io.soramitsu.iroha.models.apis;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 /**
  * Base client object.
  */
 public class BaseClient {
+    /** Endpoint url */
+    public static final String ENDPOINT_URL = "http://io.mizuki.sonoko/";
+
     /** Successful status code */
     public static final int STATUS_OK = 200;
 
     /** Client error status code */
     public static final int STATUS_BAD = 400;
+
+    /** Media type of JSON */
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpClient okHttpClient;
+
+    public BaseClient() {
+        initialize();
+    }
+
+    private void initialize() {
+        okHttpClient = new OkHttpClient();
+    }
+
+    /**
+     * Get a url.
+     *
+     * @param url url
+     * @return Response
+     * @throws IOException
+     */
+    protected String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+    /**
+     * Post to a server.
+     *
+     * @param url  url
+     * @param json Request Format Body JSON
+     * @return Response
+     * @throws IOException
+     */
+    protected String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
 }
