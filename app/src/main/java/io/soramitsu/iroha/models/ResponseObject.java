7023c103f38b7f89a7b6709a2c42e41f01810dfd
kobayashi_kento <kobaken0029@gmail.com>
2016-09-24 10:41:10 +0900

    Added getter and setter in pojo

--- app/src/main/java/io/soramitsu/iroha/models/ResponseObject.java
+++ app/src/main/java/io/soramitsu/iroha/models/ResponseObject.java
@@ -7,4 +7,20 @@ package io.soramitsu.iroha.models;
 public class ResponseObject {
     private int status;
     private String message;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
