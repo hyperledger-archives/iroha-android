efb82a772cf0ec135caac964c32215e084d78edc
kobayashi_kento <kobaken0029@gmail.com>
2016-10-07 17:29:13 +0900

    Working .gitignore

--- app/src/main/java/io/soramitsu/iroha/utils/DigestUtil.java
+++ app/src/main/java/io/soramitsu/iroha/utils/DigestUtil.java
@@ -1,17 +1,27 @@
 package io.soramitsu.iroha.utils;
 
+import android.content.Context;
+import android.security.KeyPairGeneratorSpec;
+
 import net.i2p.crypto.eddsa.EdDSAEngine;
 import net.i2p.crypto.eddsa.KeyPairGenerator;
 
 import org.apache.commons.codec.binary.Base64;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SignatureException;
+import java.util.Calendar;
+
+import javax.security.auth.x500.X500Principal;
 
 
 /**
@@ -19,6 +29,37 @@ import java.security.SignatureException;
  */
 public class DigestUtil {
     private DigestUtil() {
+    }
+
+    public static KeyPair createKeyPair_1(Context context) {
+        java.security.KeyPairGenerator kpg = null;
+        try {
+            kpg = java.security.KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+            kpg.initialize(createKeyPairGeneratorSpec(context));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+        return kpg.generateKeyPair();
+    }
+
+    public static KeyPairGeneratorSpec createKeyPairGeneratorSpec(Context context) {
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+        end.add(Calendar.YEAR, 100);
+
+        KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context)
+                .setAlias("iroha_alias")
+                .setSubject(new X500Principal(String.format("CN=%s", "iroha_alias")))
+                .setSerialNumber(BigInteger.valueOf(1000000))
+                .setStartDate(start.getTime())
+                .setEndDate(end.getTime())
+                .build();
+
+        return spec;
     }
 
     public static KeyPair createKeyPair() {
---
+++ app/src/test/java/io/soramitsu/iroha/utils/DigestUtilTest.java
@@ -0,0 +1,7 @@
+package io.soramitsu.iroha.utils;
+
+import junit.framework.TestCase;
+
+
+public class DigestUtilTest extends TestCase {
+}
