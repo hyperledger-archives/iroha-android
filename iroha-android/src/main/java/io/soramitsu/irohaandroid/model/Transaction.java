ac5da6cd292edadb25979dfdc42b57f8e0e89c94
kobayashi_kento <kobaken0029@gmail.com>
2016-12-17 07:29:00 +0900

    Deleted createMocks

--- iroha-android/src/main/java/io/soramitsu/irohaandroid/model/Transaction.java
+++ iroha-android/src/main/java/io/soramitsu/irohaandroid/model/Transaction.java
@@ -4,7 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -18,65 +17,65 @@ public class Transaction implements Serializable {
     public String signature;
     public long timestamp;
 
-    public static ArrayList<Transaction> createMock() {
-        ArrayList<Transaction> transactions = new ArrayList<>();
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.timestamp = System.currentTimeMillis() / 1000 - 60;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.timestamp = System.currentTimeMillis() / 1000 - 2592000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.timestamp = System.currentTimeMillis() / 1000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.timestamp = System.currentTimeMillis() / 1000 - 86400;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.timestamp = System.currentTimeMillis() / 1000 - 600;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.timestamp = System.currentTimeMillis() / 1000 - 259200;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.timestamp = System.currentTimeMillis() / 1000 - 7200;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.timestamp = System.currentTimeMillis() / 1000 - 31536000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.timestamp = System.currentTimeMillis() / 1000 - 50400;
-        }});
-
-        return transactions;
-    }
+//    public static ArrayList<Transaction> createMock() {
+//        ArrayList<Transaction> transactions = new ArrayList<>();
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "200";
+//            this.params.sender = "test2";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 60;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "200";
+//            this.params.sender = "test2";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 2592000;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "100";
+//            this.params.sender = "test1";
+//            this.timestamp = System.currentTimeMillis() / 1000;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "300";
+//            this.params.sender = "test3";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 86400;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "300";
+//            this.params.sender = "test3";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 600;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "100";
+//            this.params.sender = "test1";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 259200;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "100";
+//            this.params.sender = "test1";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 7200;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "300";
+//            this.params.sender = "test3";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 31536000;
+//        }});
+//        transactions.add(new Transaction(){{
+//            this.params = new OperationParameter();
+//            this.params.value = "200";
+//            this.params.sender = "test2";
+//            this.timestamp = System.currentTimeMillis() / 1000 - 50400;
+//        }});
+//
+//        return transactions;
+//    }
 
     public String modifyDisplayDate() {
         long now = Calendar.getInstance().getTimeInMillis() / 1000;
