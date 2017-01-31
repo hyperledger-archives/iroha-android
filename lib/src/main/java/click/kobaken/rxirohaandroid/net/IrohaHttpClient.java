39fbac144111ecb2b3385920965704b8b7c75455
Kento Kobayashi <kobaken0029@gmail.com>
2017-02-01 07:00:46 +0900

    Deleted unused files

--- lib/src/main/java/click/kobaken/rxirohaandroid/net/IrohaHttpClient.java
+++ lib/src/main/java/click/kobaken/rxirohaandroid/net/IrohaHttpClient.java
@@ -18,28 +18,60 @@ limitations under the License.
 package click.kobaken.rxirohaandroid.net;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import click.kobaken.rxirohaandroid.Iroha;
+import okhttp3.Headers;
+import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class IrohaHttpClient {
-    private static final IrohaHttpClient irohaClient  = new IrohaHttpClient();
+    private static IrohaHttpClient irohaClient  = new IrohaHttpClient();
 
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private final OkHttpClient client;
 
     private IrohaHttpClient() {
-        client = new OkHttpClient.Builder().build();
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Map<String, String> headerMap = new HashMap<>();
+                headerMap.put("Accept", "application/json");
+                headerMap.put("Content-type", "application/json");
+
+                //header設定
+                Request request = original.newBuilder()
+                        .headers(Headers.of(headerMap))
+                        .method(original.method(), original.body())
+                        .build();
+
+                return chain.proceed(request);
+            }
+        });
+
+        //ログ出力設定
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        httpClient.addInterceptor(logging);
+
+        client = httpClient.build();
     }
 
     public static IrohaHttpClient getInstance() {
         return irohaClient;
     }
+
+
 
     public Response call(Request request) throws IOException {
         return client.newCall(request).execute();
