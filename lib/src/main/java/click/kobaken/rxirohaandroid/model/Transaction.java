e55e0e9e1b14caec8b8a2aeb8d38cce9f95e37b0
Kento Kobayashi <kobaken0029@gmail.com>
2017-02-01 23:43:29 +0900

    Fixed Transaction according to UnitTest

--- lib/src/main/java/click/kobaken/rxirohaandroid/model/Transaction.java
+++ lib/src/main/java/click/kobaken/rxirohaandroid/model/Transaction.java
@@ -19,7 +19,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -33,9 +32,15 @@ public class Transaction implements Serializable {
     public String signature;
 
     public String modifyDisplayDate() {
+        if (this.params == null) {
+            throw new NullPointerException("OperationParameter should not be null");
+        }
+
         long now = Calendar.getInstance().getTimeInMillis() / 1000;
         long sec = now - this.params.timestamp;
-        if (sec <= 0) {
+        if (sec < 0) {
+            return "future";
+        } else if (sec == 0) {
             return "now";
         } else if (sec < 60) {
             return sec + "sec";
@@ -57,67 +62,10 @@ public class Transaction implements Serializable {
     }
 
     public boolean isSender(String publicKey) {
+        if (params == null || params.sender == null) {
+            throw new NullPointerException("OperationParameter should not be null");
+        }
         return params.sender.equals(publicKey);
-    }
-
-    public static ArrayList<Transaction> createMock() {
-        ArrayList<Transaction> transactions = new ArrayList<>();
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 60;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 2592000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.params.timestamp = System.currentTimeMillis() / 1000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 86400;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 600;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 259200;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "100";
-            this.params.sender = "test1";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 7200;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "300";
-            this.params.sender = "test3";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 31536000;
-        }});
-        transactions.add(new Transaction(){{
-            this.params = new OperationParameter();
-            this.params.value = "200";
-            this.params.sender = "test2";
-            this.params.timestamp = System.currentTimeMillis() / 1000 - 50400;
-        }});
-
-        return transactions;
     }
 
     public static class OperationParameter implements Serializable {
