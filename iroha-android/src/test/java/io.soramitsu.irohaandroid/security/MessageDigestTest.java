f1939c08822ca3eab45841c685c062c113a65ae8
kobayashi_kento <kobaken0029@gmail.com>
2017-05-29 19:28:14 +0900

    Fixed MessageDigestTest

--- iroha-android/src/test/java/io.soramitsu.irohaandroid/security/MessageDigestTest.java
+++ iroha-android/src/test/java/io.soramitsu.irohaandroid/security/MessageDigestTest.java
@@ -30,23 +30,26 @@ import static org.junit.Assert.assertThat;
  */
 public class MessageDigestTest {
 
+    private String message;
+
+    public MessageDigestTest() {
+        this.message = "test";
+    }
+
     @Test
     public void digest_sha3_256() throws Exception {
-        final String message = "test";
         final String expected = "36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80";
         assertThat(MessageDigest.digest(message, SHA3_256), is(expected));
     }
 
     @Test
     public void digest_sha3_384() throws Exception {
-        final String message = "test";
         final String expected = "e516dabb23b6e30026863543282780a3ae0dccf05551cf0295178d7ff0f1b41eecb9db3ff219007c4e097260d58621bd";
         assertThat(MessageDigest.digest(message, SHA3_384), is(expected));
     }
 
     @Test
     public void digest_sha3_512() throws Exception {
-        final String message = "test";
         final String expected = "9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14";
         assertThat(MessageDigest.digest(message, SHA3_512), is(expected));
     }
