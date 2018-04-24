596550fa80e698619edafe24705e7dc09cc00936
bulatmukhutdinov <Bulatmm@yandex.ru>
2018-04-24 10:53:21 +0300

    Iroha Android Bindings
    Signed-off-by: bulatmukhutdinov <Bulatmm@yandex.ru>

---
+++ iroha-android-sample/src/main/java/jp/co/soramitsu/iroha/android/sample/Constants.java
@@ -0,0 +1,13 @@
+package jp.co.soramitsu.iroha.android.sample;
+
+public interface Constants {
+    String PUB_KEY = "889f6b881e331be21487db77dcf32c5f8d3d5e8066e78d2feac4239fe91d416f";
+    String PRIV_KEY = "0f0ce16d2afbb8eca23c7d8c2724f0c257a800ee2bbd54688cec6b898e3f7e33";
+    String CREATOR = "admin@test";
+    long TX_COUNTER = 1;
+    long QUERY_COUNTER = 1;
+    String DOMAIN_ID = "test";
+    int CONNECTION_TIMEOUT_SECONDS = 20;
+    int MAX_ACCOUNT_DETAILS_SIZE = 32;
+    String ACCOUNT_DETAILS = "account_details";
+}
